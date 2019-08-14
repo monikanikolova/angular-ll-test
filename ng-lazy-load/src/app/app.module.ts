eae00f38b40d369642d8955874c1c2eb2c47653e
Monika G Nikolova <44558797+monikanikolova@users.noreply.github.com>
2019-08-14 10:23:36 -0400

    created 2 components, set up routing, set up lazy loading

--- ng-lazy-load/src/app/app.module.ts
+++ ng-lazy-load/src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+// import { ViewdetailsComponent} from "./viewdetails/viewdetails.component"
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "viewdetails",
+    loadChildren: "src/app/viewdetails/viewdetails.module#ViewdetailsModule"
+  },
+  {
+    path: "about",
+    loadChildren: "src/app/about/about.module#AboutModule"
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
